1c272744a826a9484c07aa68bf632abd60e4182f
Raphael Barreto <tnt.raphael@gmail.com>
2020-07-26 11:53:54 -0300

    Added ESLint + Prettier configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-   <h1>Hello World</h1>
-  );
-}
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return <h1>Hello World</h1>;
+};
 
 export default App;
