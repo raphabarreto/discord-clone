74a5e6e9d36a79bbe7b18c3a6ae98b55f20492f8
Raphael Barreto <tnt.raphael@gmail.com>
2020-07-27 16:46:27 -0300

    Added ChannelData and Channeld Message Components

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,52 @@
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        {Array.from(Array(5).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="Raphael Barreto"
+            date="30/11/2020"
+            content="Hoje é meu aniversário"
+          />
+        ))}
+
+        <ChannelMessage
+          author="Diego Fernandes"
+          date="30/11/2020"
+          content={
+            <>
+              <Mention>@Raphael Bareto</Mention>, me carrega no lolzinho de
+              novo?
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversar em #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
